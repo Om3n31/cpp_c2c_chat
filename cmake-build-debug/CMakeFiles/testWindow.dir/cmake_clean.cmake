c06cee671a57d5ac5d52b9ccd9ce6382dfafad25
m3 <nemog@hotmail.fr>
2021-04-21 22:33:44 +0200

    fc

---
+++ cmake-build-debug/CMakeFiles/testWindow.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testWindow.dir/main.cpp.obj"
+  "libtestWindow.dll.a"
+  "testWindow.exe"
+  "testWindow.exe.manifest"
+  "testWindow.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testWindow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
